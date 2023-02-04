e427a44a571ebb0618049fbc70cd3d39a04ff527
aadil42 <adilkelawala@gmail.com>
2023-02-05 02:33:03 +0530

    rendering all the data-items

--- src/Components/Data-list/DataList.js
+++ src/Components/Data-list/DataList.js
@@ -1,13 +1,15 @@
 
 import SingleData from "./SingleData";
 
+import {v4 as  uuidv4} from 'uuid';
+
 const DataList = (props) => {
 
     const data = props.data;
     return (
         <div>
             {data.map((singleData) => {
-             return <SingleData user={singleData.name} age={singleData.age} />
+             return <SingleData key={uuidv4()} user={singleData.name} age={singleData.age} />
             })}    
         </div>
     );
