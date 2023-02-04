19563bac8e14051c20eb24a3f5ccfaff8e2006ec
aadil42 <adilkelawala@gmail.com>
2023-02-05 01:56:58 +0530

    linking SingleData component in the DataList component and looping over all the data

--- src/Components/Data-list/DataList.js
+++ src/Components/Data-list/DataList.js
@@ -1,12 +1,14 @@
 
+import SingleData from "./SingleData";
+
 const DataList = (props) => {
 
     const data = props.data;
     return (
         <div>
-            {/* {data.map((singleData) => {
-
-            })}     */}
+            {data.map((singleData) => {
+             return <SingleData user={singleData.name} age={singleData.age} />
+            })}    
         </div>
     );
 }
---
+++ src/Components/Data-list/SingleData.js
@@ -0,0 +1,11 @@
+
+const SingleData = (props) => {
+
+    return (
+        <div>
+            {props.user}----{props.age}
+        </div>
+    )
+}
+
+export default SingleData;
