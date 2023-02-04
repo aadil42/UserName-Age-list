90ff565ca5d1f495fc5b14efd6d869e5100d721a
aadil42 <adilkelawala@gmail.com>
2023-02-05 01:18:05 +0530

    added form html data

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,9 +1,23 @@
 
+import './Form.css';
+
 const Form = () => {
 
     return (
         <div>
-            form
+            <form>
+            <label htmlFor="username">Username</label>
+            <input
+                id="username"
+                type="text"
+            />
+            <label htmlFor="age">Age (Years)</label>
+            <input
+                id="age"
+                type="number"
+            />
+            <button type="submit"> submit </button>
+            </form>
         </div>
     );
 
