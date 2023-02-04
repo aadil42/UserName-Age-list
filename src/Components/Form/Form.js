410e5fec5b71a6a40f8a3bf13eb16ae36128b88f
aadil42 <adilkelawala@gmail.com>
2023-02-05 02:31:52 +0530

    adding items on submit

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,15 +1,30 @@
+import { useState } from "react";
 
 import './Form.css';
 
-const Form = () => {
+const Form = (props) => {
+
+    const [user, setUser] = useState("");
+    const [age, setAge] = useState("");
 
     const addUser = (e) => {
         e.preventDefault(); // will  not load the page when form is submitted.
-        if(validateData()) {
+        if(validateData(user, age)) {
+            console.log('hehe');
+            props.appendTolist(user,age);
             // add user
+        } else {
+            alert('invalid input');
+            // invalid input // show error
         }
     }
 
+    const updateUser = (event) => {
+        setUser(event.target.value);
+    }
+    const updateAge = (event) => {
+        setAge(event.target.value);        
+    }
     const validateData = (user, age) => {
         const isValid = user.trim() && age.trim() && true;
         return isValid;   
@@ -22,11 +37,15 @@ const Form = () => {
             <input
                 id="username"
                 type="text"
+                value={user}
+                onChange={updateUser}
             />
             <label htmlFor="age">Age (Years)</label>
             <input
                 id="age"
                 type="number"
+                value={age}
+                onChange={updateAge}
             />
             <button type="submit"> submit </button>
             </form>
