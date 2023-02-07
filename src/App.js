056d5a893eef04cbe40074bd7c80322ad5894882
aadil42 <adilkelawala@gmail.com>
2023-02-08 03:19:56 +0530

    added notes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
 import React, { useState } from 'react';
+// notes
+// comonent one (form input)
+// 	username field(text)
+// 	age field(number)
+// 	button 
+// 		functionalities			
+// 			if any of the field is empty show the error.
+// 			add user when clicking add button or entering 
 
+// -----------------------------------------------done-------------------------------------------
+
+// component two (list of  data)
+// 	sub container which holds the individual data
+// 		functionalities
+// 			no functionalities.
+
+// -----------------------------------------------done--------------------------------------------
+
+// component three (error component)
+// 	This will display hard coded error 
+// 		functionalities
+// 			it will have button "okay" when clicking on it it will hide the error component 
+// 			clicking anywhere except the error component will also hide the component 
+
+
+
+
+// add styling.
+
+
+	
+	
 // imports 
 import Form from './Components/Form/Form';
 import DataList from './Components/Data-list/DataList';
@@ -20,9 +51,11 @@ function App() {
       ];
     });
   }
-
+  const hideError = () => {
+    setIsValid(false);
+  }
   return (
-    <div>
+    <div onClick={hideError}>
         <Form setIsValid={setIsValid} appendTolist={addToList} />
         <DataList data={data} />
         {!isValid && <InvalidError setIsValid={setIsValid} />}
