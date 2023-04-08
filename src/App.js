02103d55f654144a4bd68ca5002faefe57b4d388
aadil42 <adilkelawala@gmail.com>
2023-04-08 17:17:29 +0530

    emptying App file and displaying simple text

--- src/App.js
+++ src/App.js
@@ -1,65 +1,9 @@
 import React, { useState } from 'react';
-// notes
-// comonent one (form input)
-// 	username field(text)
-// 	age field(number)
-// 	button 
-// 		functionalities			
-// 			if any of the field is empty show the error.
-// 			add user when clicking add button or entering 
-
-// -----------------------------------------------done-------------------------------------------
-
-// component two (list of  data)
-// 	sub container which holds the individual data
-// 		functionalities
-// 			no functionalities.
-
-// -----------------------------------------------done--------------------------------------------
-
-// component three (error component)
-// 	This will display hard coded error 
-// 		functionalities
-// 			it will have button "okay" when clicking on it it will hide the error component 
-// 			clicking anywhere except the error component will also hide the component 
-
-/// ------------------------------------------done------------------------------------
-
-// add styling.
-
-// ----------------------------------------done---------------------------------
-
 	
-	
-// imports 
-import Form from './Components/Form/Form';
-import DataList from './Components/Data-list/DataList';
-import InvalidError from './Components/Error-components/InvalidError';
 function App() {
 
-  const [data, setData] = useState([]);
-  const [isValid, setIsValid] = useState(true);
-
-  const addToList = (user, age) => {
-    setData((data) => {
-      return [
-        ...data,
-        {
-          name: user,
-          age: age
-        }
-      ];
-    });
-  }
-  const hideError = () => {
-    setIsValid(true);
-  }
   return (
-    <div className="app-container" onClick={hideError}>
-        <Form setIsValid={setIsValid} appendTolist={addToList} />
-          <DataList data={data} /> 
-        {!isValid && <InvalidError setIsValid={setIsValid} />}
-    </div>
+    <div>test</div>
   );
 }
 
