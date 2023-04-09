a22138ca1e2c79a5011041b5ef854828902935a2
aadil42 <adilkelawala@gmail.com>
2023-04-09 16:32:53 +0530

    adding ul parent for lis

--- src/Components/Users/UsersList.js
+++ src/Components/Users/UsersList.js
@@ -9,9 +9,11 @@ import styles from './UsersList.module.css';
 const UsersList = (props) => {
     return (
         <Card className={styles.users}>
+            <ul>
             {props.allUsers.map((user) => {
                 return <li key ={user.id}>Hello {user.name}! You are {user.age} years old.</li>;
             })}
+            </ul>
         </Card>
     );
 }
